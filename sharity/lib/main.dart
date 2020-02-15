f1e6f71067e98bea1c37e5f1ed10b09734f1b233
Tyler Iams <tyleriams@Tylers-MacBook-Pro-4.local>
2020-02-15 15:15:25 -0800

    tab bar

--- sharity/lib/main.dart
+++ sharity/lib/main.dart
@@ -13,13 +13,76 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-        backgroundColor: Colors.indigo,
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
+      home: SharityHomePage(title: 'Sharity')
+    );
+  }
+}
+
+
+class SharityHomePage extends StatefulWidget {
+  final String title;
+
+  SharityHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  _SharityHomePageState createState() => _SharityHomePageState();
+}
+
+class _SharityHomePageState extends State<SharityHomePage>
+    with TickerProviderStateMixin {
+
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = new TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      bottomNavigationBar: Material(
+        color: Colors.blueGrey,
+        child: TabBar(
+          controller: _tabController,
+          indicatorColor: Colors.teal,
+          unselectedLabelColor: Colors.black54,
+          tabs: <Widget>[
+            Tab(
+              icon: Icon(Icons.home),
+            ),
+            Tab(
+              icon: Icon(Icons.email),
+            ),
+            Tab(
+              icon: Icon(Icons.settings),
+            ),
+          ]
+        )
+      ),
+      body: TabBarView(
+        children: <Widget>[
+          Center(
+            child: Text("Home"),
+          ),
+          Center(
+            child: Text("Email"),
+          ),
+          Center(
+            child: Text("Settings"),
+          )
+        ],
+        controller: _tabController,
       ),
     );
   }
